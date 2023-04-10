726f652138709b211da0184b627f26680f964fea
Jean-Michel <jmichel.gambard@gmail.com>
2023-04-10 20:46:34 +0200

    feat: create BeerDetailsPage

---
+++ src/app/beers/[beerID]/page.tsx
@@ -0,0 +1,52 @@
+import { BeerDetails } from "@/components/BeerDetails";
+import { beerIDSchema, realBeersRepository } from "@/domain/beersRepository";
+import { createMetadata } from "@/utils/createMetadata";
+import { notFound } from "next/navigation";
+import Script from "next/script";
+import { Product, WithContext } from "schema-dts";
+
+interface BeerDetailsPageProps {
+  params: {
+    beerID: string;
+  };
+}
+
+export async function generateMetadata({ params }: BeerDetailsPageProps) {
+  try {
+    const beerID = beerIDSchema.parse(+params.beerID);
+    const beer = await realBeersRepository.getBeer(beerID);
+
+    return createMetadata({
+      type: "article",
+      title: beer.name,
+      description: beer.description,
+      images: beer.image_url ? [beer.image_url] : [],
+    });
+  } catch {
+    return {};
+  }
+}
+
+export default async function BeerDetailsPage({ params }: BeerDetailsPageProps) {
+  const beerID = beerIDSchema.safeParse(+params.beerID);
+  if (!beerID.success) notFound();
+  const beer = await realBeersRepository.getBeer(beerID.data).catch(notFound);
+  const jsonLD: WithContext<Product> = {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    name: beer.name,
+    description: beer.description,
+    image: beer.image_url ? [beer.image_url] : [],
+  };
+
+  return (
+    <>
+      <Script
+        id="beer-json-ld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLD) }}
+      />
+      <BeerDetails beerID={beerID.data} />
+    </>
+  );
+}
