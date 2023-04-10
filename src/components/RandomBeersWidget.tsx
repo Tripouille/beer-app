435854e5b1d7845deeafe475891e9cf6fd432bfd
jgambard <jmichel.gambard@gmail.com>
2023-04-10 18:10:08 +0200

    feat: create random beers widget skeleton (#7)

--- src/components/RandomBeersWidget.tsx
+++ src/components/RandomBeersWidget.tsx
@@ -6,7 +6,11 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 import Link from "next/link";
 import { ROUTES } from "@/utils/routes";
-import { leftSlideAnimation, rightSlideAnimation } from "@/styles/animations";
+import {
+  leftSlideAnimation,
+  rightSlideAnimation,
+  twinkleAnimation,
+} from "@/styles/animations";
 import getBeerImageSrc from "@/domain/beersUtils";
 
 export const RandomBeersWidget = () => {
@@ -36,7 +40,8 @@ export const RandomBeerCard = () => {
     isError,
   } = useRandomBeerQuery(randomBeerID, { refetchInterval: 10000 });
 
-  if (isLoading) return null;
+  if (isLoading)
+    return <BeerCardSkeleton aria-busy="true" data-testid="loading-state" />;
   if (isError) return null;
 
   return (
@@ -106,5 +111,16 @@ const BeerCard = styled("article", {
   time: {
     gridArea: "first_brewed",
     placeSelf: "start",
+  },
+});
+
+const BeerCardSkeleton = styled("div", {
+  width: "var(--beer-card-min-width)",
+  height: "var(--beer-card-min-height)",
+  borderRadius: "var(--beer-card-border-radius)",
+  background: theme.colors.neutral400,
+
+  "@media (prefers-reduced-motion: no-preference)": {
+    animation: `${twinkleAnimation} 1s linear infinite alternate`,
   },
 });
--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -9,3 +9,8 @@ export const leftSlideAnimation = keyframes({
   "0%": { transform: "translateX(100%)" },
   "100%": { transform: "translateX(0)" },
 });
+
+export const twinkleAnimation = keyframes({
+  "0%": { opacity: 0.4 },
+  "100%": { opacity: 1 },
+});
