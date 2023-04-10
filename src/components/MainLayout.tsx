c104043db84df1b218c3b5e2142b06102f689ae3
jgambard <jmichel.gambard@gmail.com>
2023-04-10 16:53:30 +0200

    feat: create main layout (#2)

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { styled, theme } from "@/styles/config";
+import { ROUTES } from "@/utils/routes";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+
+export const MainLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <MainLayoutContainer>
+      <header>
+        <h1>
+          <Link href={ROUTES.HOME()}>Punk App 🍺</Link>
+        </h1>
+      </header>
+      {children}
+    </MainLayoutContainer>
+  );
+};
+
+const MainLayoutContainer = styled("main", {
+  "--main-layout-max-width": "50em",
+  display: "flex",
+  flexFlow: "column",
+  alignItems: "center",
+  gap: theme.spaces.md,
+  maxWidth: "var(--main-layout-max-width)",
+  margin: "auto",
+
+  "*": {
+    boxSizing: "border-box",
+    color: theme.colors.neutral700,
+  },
+
+  "p, h1, h2, h3, h4, h5": {
+    margin: 0,
+  },
+
+  ul: {
+    padding: 0,
+  },
+
+  header: {
+    h1: {
+      color: theme.colors.identity700,
+    },
+  },
+});
